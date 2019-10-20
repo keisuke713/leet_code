3de74fe0a71569f819a04c018192e0f20a4987e5
keisuke713 <nebakei.tkb713@gmail.com>
2019-10-20 17:48:08 +0900

    Find_length_of_lcis

--- lib/find_length_of_lcis.rb
+++ lib/find_length_of_lcis.rb
@@ -1,6 +1,10 @@
 def find_length_of_lcis(nums)
   return 1 if nums.count(nums.first) == nums.count
-  # 一つ目のexpectを通るおようにする
+  increase_num = increase_num(nums)
+  increase_num.uniq.map { |num| increase_num.count num }.sort!.last + 1
+end
+
+def increase_num(nums)
   nums[1..-1].map.with_index(1) do |n, i|
     n - nums[i -1]
   end
